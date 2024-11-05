f2771f9349a78168011fd7509cec187467ed1b03
Yuragranovsky <yuragranovsky@gmail.com>
2024-11-05 12:34:13 +0200

    HW #37 Solution

--- reverse-length-client/src/main/java/telran/net/Main.java
+++ reverse-length-client/src/main/java/telran/net/Main.java
@@ -1,8 +1,47 @@
 package telran.net;
+import java.util.HashSet;
+import java.util.List;
 
+import org.json.JSONObject;
+
+import telran.view.*;
 public class Main {
+    static ReverseLengthClient client;
+
     public static void main(String[] args) {
-      //TODO
-      //client application
+      Item[] items = {
+            Item.of("start session", Main::startSession),
+            Item.of("exit", Main::exit, true)
+      };
+      Menu menu = new Menu("Echo Application", items);
+      menu.perform(new StandardInputOutput());
+    }
+    static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname");
+        int port = io.readNumberRange("Enter port", "Wrong port", 3000, 50000).intValue();
+        if(client != null) {
+            client.close();
+        }
+        client = new ReverseLengthClient(host, port);
+        Menu menu = new Menu("Run Session",
+         Item.of("session", Main::sessionProcessing),
+          Item.ofExit());
+         menu.perform(io);
+
+    }
+    static void sessionProcessing(InputOutput io) {
+       HashSet<String> types = new HashSet<>(List.of("reverse", "length"));
+        String type = io.readStringOptions("Enter operation type " + types, "Wrong type", types);
+        String string = io.readString("Enter any string");
+        JSONObject jsonObj = new JSONObject();
+        jsonObj.put("type",type);
+        jsonObj.put("string", string);
+        String response = client.sendAndReceive(jsonObj.toString());
+        io.writeLine(response);
+    }
+    static void exit(InputOutput io) {
+        if(client != null) {
+            client.close();
+        }
     }
 }
--- reverse-length-server/src/main/java/telran/net/Main.java
+++ reverse-length-server/src/main/java/telran/net/Main.java
@@ -1,8 +1,46 @@
 package telran.net;
 
+import java.net.*;
+
+import org.json.JSONObject;
+
+import java.io.*;
+
 public class Main {
-    public static void main(String[] args) {
-        //TODO
-        //Server application
+    private static final int PORT = 4000;
+
+    public static void main(String[] args) throws Exception {
+        try (ServerSocket serverSocket = new ServerSocket(PORT);) {
+            System.out.println("Server is listening on port " + PORT);
+            while (true) {
+                Socket socket = serverSocket.accept();
+                runSession(socket);
+            }
+        }
+    }
+
+    private static void runSession(Socket socket) {
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                PrintStream writer = new PrintStream(socket.getOutputStream())) {
+            String line = "";
+            while ((line = reader.readLine()) != null) {
+                String response = getResponse(line);
+                writer.println(response);
+            }
+        } catch (Exception e) {
+            System.out.println("client closed connection abnormally");
+        }
+    }
+
+    private static String getResponse(String line) {
+        JSONObject jsonObj = new JSONObject(line);
+        String type = jsonObj.getString("type");
+        String string = jsonObj.getString("string");
+        String res = switch(type){
+            case "reverse" -> new StringBuilder(string).reverse().toString();
+            case "length" -> string.length() + "";
+            default -> type + " Wrong type";
+        };
+        return res;
     }
 }
